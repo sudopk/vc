89bc3f47cfd2b7f6e532dffe03bbc8e4ee63d9b3
Parveen Kumar <email@email.com>
2017-01-02 08:51:54 -0800

    Refactored vc converter factory

--- app/src/main/java/in/sudopk/vaishnavacalendar/retrofit/VcConverterFactory.java
+++ app/src/main/java/in/sudopk/vaishnavacalendar/retrofit/VcConverterFactory.java
@@ -38,97 +38,131 @@ public class VcConverterFactory extends Converter.Factory {
     }
 
 
-    private class VcCalendarResponseConverter implements Converter<ResponseBody, VCalendar> {
+    private static class VcCalendarResponseConverter implements Converter<ResponseBody, VCalendar> {
+
+        private List<String> mEventData;
+        private int mDate;
+
         @Override
         public VCalendar convert(final ResponseBody value) throws IOException {
             final Document document = Jsoup.parse(value.string());
             final Elements days = document.select("body center>table:nth-child(3)>tbody>tr>td>table>tbody");
             final List<DayCalendar> calendar = new ArrayList<>();
             for (final Element day : days) {
-                // each day is a <table>
-                final Elements events = day.select(">tr");
-                int date = 0;
-                final List<String> eventData = new ArrayList<>(events.size());
-                boolean firstRow = true;
-                for (final Element event : events) {
-                    int col = 0;
-                    final Elements details = event.select(">td");
-                    for (final Element detail : details) {
-                        String data = detail.text();
-                        if (firstRow && col == 1) {
-                            date = Integer.parseInt(data);
-                        } else {
-                            final Elements imgs = detail.select("img");
-                            if (imgs.size() > 0) {
-                                final String src = imgs.attr("src");
-                                //TODO: Use res strings
-                                if ("amavasya.gif".equals(src)) {
-                                    data = "[New moon]";
-                                } else if ("purnima.gif".equals(src)) {
-                                    data = "[Full moon]";
-                                } else if ("ap.gif".equals(src)) {
-                                    data += " [Appearance]";
-                                } else if ("dis.gif".equals(src)) {
-                                    data += " [Disapperance]";
-                                }
-                            }
-                            if (!TextUtils.isEmpty(data)) {
-                                eventData.add(data);
-                            }
-                        }
-                        col++;
-                    }
-                    firstRow = false;
-                }
-                if (date != 0) {
-                    calendar.add(new DayCalendar(date, eventData));
+                final DayCalendar dayCalendar = parseDay(day);
+                if (dayCalendar != null) {
+                    calendar.add(dayCalendar);
                 }
             }
             return new VCalendar(calendar);
         }
+
+        private DayCalendar parseDay(final Element day) {
+            // each day is a <table>
+            final Elements events = day.select(">tr");
+            mDate = 0;
+            mEventData = new ArrayList<>(events.size());
+            boolean firstRow = true;
+            for (final Element event : events) {
+                parseDayEvent(event, firstRow);
+                firstRow = false;
+            }
+            if (mDate != 0) {
+                return new DayCalendar(mDate, mEventData);
+            }
+            return null;
+        }
+
+        private void parseDayEvent(final Element event, final boolean dateEvent) {
+            int col = 0;
+            final Elements details = event.select(">td");
+            for (final Element detail : details) {
+                parseEventDetail(detail, dateEvent && (col == 1));
+                col++;
+            }
+        }
+
+
+        private void parseEventDetail(final Element detail, final boolean dateEvent) {
+            String data = detail.text();
+            if (dateEvent) {
+                mDate = Integer.parseInt(data);
+            } else {
+                data += parseImgToText(detail.select("img"));
+                data = data.trim();
+                if (!TextUtils.isEmpty(data)) {
+                    mEventData.add(data);
+                }
+            }
+        }
+
+        private String parseImgToText(final Elements imgs) {
+            if (imgs.size() > 0) {
+                final String src = imgs.attr("src");
+                //TODO: Use res strings
+                if ("amavasya.gif".equals(src)) {
+                    return "[New moon]";
+                } else if ("purnima.gif".equals(src)) {
+                    return "[Full moon]";
+                } else if ("ap.gif".equals(src)) {
+                    return " [Appearance]";
+                } else if ("dis.gif".equals(src)) {
+                    return " [Disapperance]";
+                }
+            }
+            return "";
+        }
     }
 
-    private class VcLocationResponseConverter implements Converter<ResponseBody, List<Country>> {
+    private static class VcLocationResponseConverter implements Converter<ResponseBody, List<Country>> {
         private static final String PREFIX_OF_LOCATIONS_WITH_ID = "....";
+        private List<Country> mLocationData;
+
+        private static boolean isCountryName(final String locationName) {
+            return !locationName.startsWith(PREFIX_OF_LOCATIONS_WITH_ID);
+        }
 
         @Override
         public List<Country> convert(final ResponseBody value) throws IOException {
             final Document document = Jsoup.parse(value.string());
             final Elements locationGroups = document.select("select[id=CIID]");
-            List<Country> locationData = new ArrayList<>();
+            mLocationData = new ArrayList<>();
             for (final Element locationGroup : locationGroups) {
-                final Elements locations = locationGroup.select(">option");
-                // initializing it to avoid null check, but it should be overridden in the loop
-                List<Location> locationsWithId = new ArrayList<>();
-                String country = "";
-                for (final Element location : locations) {
-                    final String locationName = location.text();
-                    if (locationName.startsWith(PREFIX_OF_LOCATIONS_WITH_ID)) {
-                        // strip the prefix from locationName
-                        locationsWithId.add(
-                                new Location(
-                                        locationName.substring(PREFIX_OF_LOCATIONS_WITH_ID.length()),
-                                        location.attr("value")));
-                    } else {
-                        // add data of last country
-                        addCountryData(locationData, country, locationsWithId);
-
-                        country = locationName;
-                        locationsWithId = new ArrayList<>();
-                    }
-                }
-                addCountryData(locationData, country, locationsWithId);
+                parseLocationGroup(locationGroup);
             }
 
-            return locationData;
+            return mLocationData;
         }
 
-        private void addCountryData(final List<Country> locationData,
-                                    final String country,
+        private void parseLocationGroup(final Element locationGroup) {
+            final Elements locations = locationGroup.select(">option");
+            // initializing it to avoid null check, but it should be overridden in the loop
+            List<Location> locationsWithId = new ArrayList<>();
+            String country = "";
+            for (final Element location : locations) {
+                final String locationName = location.text();
+                if (isCountryName(locationName)) {
+                    // add data of last country
+                    addCountryData(country, locationsWithId);
+
+                    country = locationName;
+                    locationsWithId = new ArrayList<>();
+                } else {
+                    // strip the prefix from locationName
+                    locationsWithId.add(
+                            new Location(
+                                    locationName.substring(PREFIX_OF_LOCATIONS_WITH_ID.length()),
+                                    location.attr("value")));
+                }
+            }
+            addCountryData(country, locationsWithId);
+        }
+
+        private void addCountryData(final String country,
                                     final List<Location> locations) {
             if (!locations.isEmpty() && !TextUtils.isEmpty(country)) {
                 Collections.sort(locations);
-                locationData.add(new Country(country, locations));
+                mLocationData.add(new Country(country, locations));
             }
         }
     }
