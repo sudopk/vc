71f3621796390965d420c6ce2b82a370a15cbe3a
Parveen Kumar <email@email.com>
2017-05-18 20:36:37 -0700

    Convert VcConfig

---
+++ app/src/main/java/in/sudopk/vaishnavacalendar/VcConfig.kt
@@ -0,0 +1,10 @@
+package `in`.sudopk.vaishnavacalendar
+
+class VcConfig {
+    var version: VcVersion = VcVersion()
+
+    class VcVersion {
+        var soft: Int = 0
+        var hard: Int = 0
+    }
+}
