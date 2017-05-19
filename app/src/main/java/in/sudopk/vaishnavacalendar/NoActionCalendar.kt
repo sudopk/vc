cd8a695f36b47f47e486ee83bd145cc9d3338d2b
Parveen Kumar <email@email.com>
2017-05-18 21:30:23 -0700

    Convert NoActionCalendar

---
+++ app/src/main/java/in/sudopk/vaishnavacalendar/NoActionCalendar.kt
@@ -0,0 +1,25 @@
+package `in`.sudopk.vaishnavacalendar
+
+import `in`.sudopk.vaishnavacalendar.calendar.CalendarDelegate
+
+class NoActionCalendar : CalendarDelegate {
+    override fun onRefresh() {
+
+    }
+
+    override fun tryShowingCalendar() {
+
+    }
+
+    override fun onChangeLocationRequest() {
+
+    }
+
+    override fun onCalendarResponse(body: VCalendar) {
+
+    }
+
+    override fun onCalendarRequestFailed() {
+
+    }
+}
