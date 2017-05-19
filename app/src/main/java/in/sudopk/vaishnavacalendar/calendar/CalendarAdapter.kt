2245cfa70d2d9bd5dd3b11a493ddc1cd02b55471
Parveen Kumar <email@email.com>
2017-05-19 00:43:10 -0700

    Convert CalendarAdapter

---
+++ app/src/main/java/in/sudopk/vaishnavacalendar/calendar/CalendarAdapter.kt
@@ -0,0 +1,112 @@
+package `in`.sudopk.vaishnavacalendar.calendar
+
+import android.support.annotation.LayoutRes
+import android.support.v7.widget.RecyclerView
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ArrayAdapter
+import android.widget.TextView
+
+import java.util.ArrayList
+import java.util.Calendar
+import java.util.Collections
+
+import `in`.sudopk.coreandroid.CalUtil
+import `in`.sudopk.coreandroid.Layout
+import `in`.sudopk.coreandroid.SimpleList
+import `in`.sudopk.vaishnavacalendar.R
+import `in`.sudopk.vaishnavacalendar.VCalendar
+
+
+/**
+ * @param month 1 to 12
+ * *
+ * @param year  Full year e.g. 2016
+ */
+class CalendarAdapter(private val mMonth: Int, private val mYear: Int) : RecyclerView
+.Adapter<CalendarAdapter.VH>() {
+
+    private val mVCalendar: MutableList<DayCalendar>
+    private var mDateToHighlight: Int = 0
+
+
+    init {
+        mVCalendar = ArrayList<DayCalendar>()
+    }
+
+    /**
+     * @param dateToHighlight Month dates i.e. 1 to 31. Pass 0 if don't want to highlight any date
+     */
+    fun setDateToHighlight(dateToHighlight: Int) {
+        mDateToHighlight = dateToHighlight
+        notifyDataSetChanged()
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): VH {
+        val cell: Int
+        val eventLayout: Int
+        if (viewType == NOT_TODAY_VIEW_TYPE) {
+            cell = R.layout.calendar_cell
+            eventLayout = R.layout.text_view
+        } else {
+            cell = R.layout.calendar_cell_today
+            eventLayout = R.layout.text_view
+        }
+        return VH(LayoutInflater.from(parent.context)
+                .inflate(cell, parent, false), eventLayout, mMonth, mYear)
+    }
+
+    override fun getItemViewType(position: Int): Int {
+        val cal = mVCalendar[position]
+        if (cal.date == mDateToHighlight) {
+            return TODAY_VIEW_TYPE
+        } else {
+            return NOT_TODAY_VIEW_TYPE
+        }
+    }
+
+    override fun onBindViewHolder(holder: VH, position: Int) {
+        holder.onBind(mVCalendar[position])
+    }
+
+    override fun getItemCount(): Int {
+        return mVCalendar.size
+    }
+
+    fun setData(calendar: VCalendar) {
+        mVCalendar.clear()
+        mVCalendar.addAll(calendar.calendar)
+        Collections.sort(mVCalendar)
+        notifyDataSetChanged()
+    }
+
+    class VH(itemView: View, @param:LayoutRes private val mEventLayout: Int, month: Int, year: Int) : RecyclerView.ViewHolder(itemView) {
+        private val mEvents: SimpleList
+        private val mDate: TextView
+        private val mMonthCalendar: Calendar
+
+        init {
+            mDate = Layout.findViewById<TextView>(itemView, R.id.date)
+            mEvents = Layout.findViewById<SimpleList>(itemView, R.id.events)
+
+            mMonthCalendar = Calendar.getInstance()
+            mMonthCalendar.set(Calendar.MONTH, month - 1)
+            mMonthCalendar.set(Calendar.YEAR, year)
+        }
+
+        fun onBind(dayCalendar: DayCalendar) {
+            mMonthCalendar.set(Calendar.DATE, dayCalendar.date)
+            mDate.text = "${CalUtil.monthAbbreviation(mMonthCalendar)} ${dayCalendar
+                    .date}\n${CalUtil.weekDayAbbreviation(mMonthCalendar)}"
+            mEvents.setAdapter(ArrayAdapter(itemView.context,
+                    mEventLayout, dayCalendar.events))
+        }
+    }
+
+    companion object {
+        private val TAG = "CalendarAdapter"
+        private val NOT_TODAY_VIEW_TYPE = 0
+        private val TODAY_VIEW_TYPE = 1
+    }
+}
